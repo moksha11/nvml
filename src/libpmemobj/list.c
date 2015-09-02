bcd009c13ca00a1af89690e408c64076a97a88ff
“sudarsun” <sudarsun@gatech.edu>
2015-09-02 01:46:52 -0400

    adding btree_map to benchmark and other changes

--- src/libpmemobj/list.c
+++ src/libpmemobj/list.c
@@ -829,10 +829,7 @@ list_insert_new(PMEMobjpool *pop, struct list_head *oob_head,
 				next_offset, prev_offset);
 	}
 
-	/*for avoiding compilation errors*/
-	goto err_redolog;
 err_redolog:
-
 
 	if (oidp != NULL) {
 		if (OBJ_PTR_IS_VALID(pop, oidp))
